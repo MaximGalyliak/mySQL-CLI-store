45cacc064412a2b3eb9c9ea66cbe3b6412ebcf52
Adler11th <mgoftest@gmail.com>
2018-04-16 19:31:47 -0700

    added ability for manager to stock up items

--- manager.js
+++ manager.js
@@ -14,7 +14,7 @@ function manager() {
         {
             type: 'list',
             message: 'What would you like to do?',
-            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
+            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Exit'],
             name: 'choice',
         }
     ]).then(input => {
@@ -22,7 +22,9 @@ function manager() {
         switch (input.choice) {
             case 'View Products for Sale': showAll(); break;
             case 'View Low Inventory': showLow(); break;
-            case choices[2]: console.log('ok'); break;
+            case 'Add to Inventory': addQuantity(); break;
+            case 'Add New Product': addProduct(); break;
+            case 'Exit': connection.end(); break;
             default: console.log("Didn't get that...");
         }
     })
@@ -50,4 +52,48 @@ function showLow() {
     );
 }
 
+function addQuantity(){
+    connection.query(
+        'SELECT id,product_name FROM productstb ORDER BY id',
+        (err, sqlRes) => {
+            if (err) throw err;
+            INQ.prompt([
+                {
+                    type: 'list',
+                    message: 'Which product you would like to stock up?',
+                    choices: UTILZ.sqlResToArr(sqlRes, 'id', 'product_name'),
+                    name: 'productName'
+                },
+                {
+                    type: 'input',
+                    message: 'How many you want to add?',
+                    name: 'amount',
+                    validate: UTILZ.validateIfNumber
+                }
+            ]).then((inqResp) => {
+                var id = inqResp.productName.slice(0, inqResp.productName.indexOf(','));
+                connection.query(
+                    `select stock_quntity from productstb where productstb.id ="${id}"`,
+                    (err, selResp)=>{
+                        if(err) throw err;
+                        var updatedQuantity = selResp[0].stock_quntity + parseInt(inqResp.amount);
+                        console.log(updatedQuantity);
+                        connection.query(
+                            `update productstb set stock_quntity = "${updatedQuantity}" where productstb.id ="${id}"`,
+                            (error, updResp) => {
+                                if (error) throw error;
+                                console.log('Success!');
+                                manager();
+                            }
+                        );
+                    }
+                )
+            });
+        }
+    );
+}
+
+function addProduct(){
+    
+}
 module.exports = manager;
